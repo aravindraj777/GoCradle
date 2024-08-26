3e724efad71d5245ee4fbbcd0f8a50b82bb6edcb
Aravind <aravindmunna240@gmail.com>
2024-08-26 20:25:19 +0530

    Basics : Arrays , Integer Arrays and their memory allocation

--- basics/array.go
+++ basics/array.go
@@ -8,3 +8,8 @@ func printArray() {
 	arr = [3]string{"Aravind", "Sreekanth", "Arun"}
 	fmt.Println("array : ", arr)
 }
+func intArray() {
+	fmt.Println("Integer Array")
+	arr2 := [3]int{12, 3, 5}
+	fmt.Println(arr2)
+}
