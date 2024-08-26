0ce17adb581c19767ea0f56eccea7f7e794935a3
Aravind <aravindmunna240@gmail.com>
2024-08-26 20:07:55 +0530

    Basics : Arrays and function syntaxes and their implementations

---
+++ basics/array.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func printArray() {
+	fmt.Println("Print array")
+	var arr [3]string
+	arr = [3]string{"Aravind", "Sreekanth", "Arun"}
+	fmt.Println("array : ", arr)
+}
